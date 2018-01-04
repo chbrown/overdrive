f683b623fb240f971567e2b0de8ff8e019fb787d
Christopher Brown <io@henrian.com>
2018-01-04 13:10:48 -0600

    Prefer using single quotes around printf format string

--- download.sh
+++ download.sh
@@ -12,7 +12,7 @@ fi
 set -e
 
 if [[ -n "$DEBUG" ]]; then
-  printf "Entering debug (verbose) mode\n"
+  printf 'Entering debug (verbose) mode\n'
   set -x
 fi
 
@@ -27,33 +27,33 @@ odm="$1"
 
 # get the license signature
 if [[ -e "$odm.license" ]]; then
-  printf "License already acquired: %s\n" "$odm.license"
+  printf 'License already acquired: %s\n' "$odm.license"
 else
   # generate random Client ID
   ClientID=$(uuid | tr /a-z/ /A-Z/)
-  printf "Generating random ClientID=%s\n" "$ClientID"
+  printf 'Generating random ClientID=%s\n' "$ClientID"
 
   # first extract the "AcquisitionUrl"
   AcquisitionUrl=$(xmlstarlet sel -t -v '/OverDriveMedia/License/AcquisitionUrl' "$odm")
-  printf "Using AcquisitionUrl=%s\n" "$AcquisitionUrl"
+  printf 'Using AcquisitionUrl=%s\n' "$AcquisitionUrl"
 
   MediaID=$(xmlstarlet sel -t -v '/OverDriveMedia/@id' "$odm")
-  printf "Using MediaID=%s\n" "$MediaID"
+  printf 'Using MediaID=%s\n' "$MediaID"
 
   # Compute the Hash value; thanks to https://github.com/jvolkening/gloc/blob/v0.601/gloc#L1523-L1531
   RawHash="$ClientID|$OMC|$OS|ELOSNOC*AIDEM*EVIRDREVO"
-  printf "Using RawHash=%s\n" "$RawHash"
+  printf 'Using RawHash=%s\n' "$RawHash"
   Hash=$(echo -n "$RawHash" | iconv -f ASCII -t UTF-16LE | openssl dgst -binary -sha1 | base64)
-  printf "Using Hash=%s\n" "$Hash"
+  printf 'Using Hash=%s\n' "$Hash"
 
   curl -A "$UserAgent" "$AcquisitionUrl?MediaID=$MediaID&ClientID=$ClientID&OMC=$OMC&OS=$OS&Hash=$Hash" > "$odm.license"
 fi
 License=$(cat "$odm.license")
-printf "Using License=%s\n" "$License"
+printf 'Using License=%s\n' "$License"
 
 # the license XML specifies a default namespace, which the XPath expression must also reference
 ClientID=$(xmlstarlet sel -N ol=http://license.overdrive.com/2008/03/License.xsd -t -v '/ol:License/ol:SignedInfo/ol:ClientID' "$odm.license")
-printf "Using ClientID=%s from License\n" "$ClientID"
+printf 'Using ClientID=%s from License\n' "$ClientID"
 
 extractMetadata() {
   # the Metadata XML is nested as CDATA inside the the root OverDriveMedia element;
@@ -64,26 +64,26 @@ extractMetadata() {
 
 # extract the title and author
 Title=$(extractMetadata "$odm" | xmlstarlet sel -t -v '//Title' | tr -Cs '[:alnum:] ._-' -)
-printf "Using Title=%s\n" "$Title"
+printf 'Using Title=%s\n' "$Title"
 Author=$(extractMetadata "$odm" | xmlstarlet sel -t -v "//Creator[@role='Author'][position()<=3]/text()" | tr '\n' + | sed 's/+/, /g')
-printf "Using Author=%s\n" "$Author"
+printf 'Using Author=%s\n' "$Author"
 
 # prepare to download the parts
 baseurl=$(xmlstarlet sel -t -v '//Protocol[@method="download"]/@baseurl' "$odm")
 
 dir="$Author - $Title"
-printf "Creating directory %s\n" "$dir"
+printf 'Creating directory %s\n' "$dir"
 mkdir -p "$dir"
 
 while read -r path; do
   # delete from path up until the last hyphen to the get Part0N.mp3 suffix
   suffix=${path##*-}
   output="$dir/$Title-$suffix"
-  printf "Downloading %s\n" "$output"
+  printf 'Downloading %s\n' "$output"
   curl -L \
     -A "$UserAgent" \
     -H "License: $License" \
     -H "ClientID: $ClientID" \
     --compressed -o "$output" \
     "$baseurl/$path"
-done < <(xmlstarlet sel -t -v '//Part/@filename' "$odm" | tr '\\' / | sed -e "s/{/%7B/" -e "s/}/%7D/")
+done < <(xmlstarlet sel -t -v '//Part/@filename' "$odm" | tr \\ / | sed -e "s/{/%7B/" -e "s/}/%7D/")
--- return.sh
+++ return.sh
@@ -10,8 +10,8 @@ fi
 
 # Read the EarlyReturnURL tag from the input odm file
 EarlyReturnURL=$(xmlstarlet sel -t -v '/OverDriveMedia/EarlyReturnURL' "$1")
-printf "Using EarlyReturnURL=%s\n" "$EarlyReturnURL"
+printf 'Using EarlyReturnURL=%s\n' "$EarlyReturnURL"
 
 curl -A "OverDrive Media Console/3.7.0.28 iOS/10.3.3" "$EarlyReturnURL"
 # that response doesn't have a newline, so one more superfluous log to clean up:
-printf "\nFinished returning book\n"
+printf '\nFinished returning book\n'
